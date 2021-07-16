b5bbae9233b79ee67c726c27a87d11d9cbf5de0d
johnisthebest <48302154+johnisthebest@users.noreply.github.com>
2021-07-16 00:23:29 -0400

    Update kekkles.js

--- kekkles.js
+++ kekkles.js
@@ -1,28 +1,56 @@
-// Make sure "http://localhost:5000" is in your applications redirect URIs!
-var loginRequest = new LoginRequest(
-  new Uri("http://localhost:5000"),
-  "ClientId",
-  LoginRequest.ResponseType.Code
-)
-{
-  Scope = new[] { Scopes.PlaylistReadPrivate, Scopes.PlaylistReadCollaborative }
+
+const CLIENT_ID = "81472223417c43ddba6f616bded26b4e"; // insert your client id here from spotify
+const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
+const REDIRECT_URL_AFTER_LOGIN = "http://johnzhao.ca";
+const SPACE_DELIMITER = "%20";
+const SCOPES = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+  "playlist-read-private",
+];
+const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER);
+
+/* 
+http://localhost:3000/webapp#access_token=ABCqxL4Y&token_type=Bearer&expires_in=3600
+*/
+const getReturnedParamsFromSpotifyAuth = (hash) => {
+  const stringAfterHashtag = hash.substring(1);
+  const paramsInUrl = stringAfterHashtag.split("&");
+  const paramsSplitUp = paramsInUrl.reduce((accumulater, currentValue) => {
+    console.log(currentValue);
+    const [key, value] = currentValue.split("=");
+    accumulater[key] = value;
+    return accumulater;
+  }, {});
+
+  return paramsSplitUp;
 };
-var uri = loginRequest.ToUri();
-// Redirect user to uri via your favorite web-server
 
-// This method should be called from your web-server when the user visits "http://localhost:5000"
-public Task GetCallback(string code)
-{
-  var response = await new OAuthClient().RequestToken(
-    new AuthorizationCodeTokenRequest("ClientId", "ClientSecret", code, "http://localhost:5000")
+const WebApp = () => {
+  useEffect(() => {
+    if (window.location.hash) {
+      const { access_token, expires_in, token_type } =
+        getReturnedParamsFromSpotifyAuth(window.location.hash);
+
+      localStorage.clear();
+
+      localStorage.setItem("accessToken", access_token);
+      localStorage.setItem("tokenType", token_type);
+      localStorage.setItem("expiresIn", expires_in);
+    }
+  });
+
+  const handleLogin = () => {
+    window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
+  };
+
+  return (
+    <div className="container">
+      <h1>hi</h1>
+      <button onClick={handleLogin}>login to spotify</button>
+      <SpotifyGetPlaylists />
+    </div>
   );
+};
 
-  var spotify = new SpotifyClient(response.AccessToken);
-  // Also important for later: response.RefreshToken
-}
-
-var newResponse = await new OAuthClient().RequestToken(
-  new AuthorizationCodeRefreshRequest("ClientId", "ClientSecret", response.RefreshToken)
-);
-
-var spotify = new SpotifyClient(newResponse.AccessToken);
+export default WebApp;
