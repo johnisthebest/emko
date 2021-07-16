72cfcb935b65db88a354bc0bbc38bf3caa107ff1
johnisthebest <48302154+johnisthebest@users.noreply.github.com>
2021-07-15 22:50:35 -0400

    Update kekkles.js

--- kekkles.js
+++ kekkles.js
@@ -9,3 +9,20 @@ var loginRequest = new LoginRequest(
 };
 var uri = loginRequest.ToUri();
 // Redirect user to uri via your favorite web-server
+
+// This method should be called from your web-server when the user visits "http://localhost:5000"
+public Task GetCallback(string code)
+{
+  var response = await new OAuthClient().RequestToken(
+    new AuthorizationCodeTokenRequest("ClientId", "ClientSecret", code, "http://localhost:5000")
+  );
+
+  var spotify = new SpotifyClient(response.AccessToken);
+  // Also important for later: response.RefreshToken
+}
+
+var newResponse = await new OAuthClient().RequestToken(
+  new AuthorizationCodeRefreshRequest("ClientId", "ClientSecret", response.RefreshToken)
+);
+
+var spotify = new SpotifyClient(newResponse.AccessToken);
