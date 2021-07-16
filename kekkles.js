bdb32206c49166955da40433f33bfd67fedaf490
johnisthebest <48302154+johnisthebest@users.noreply.github.com>
2021-07-15 22:49:05 -0400

    Add files via upload

--- kekkles.js
+++ kekkles.js
@@ -1,1 +1,32 @@
+var arr = ['a','b','c','d','e','f'];
+
+lng = arr.length;
+
+do 
+{
+n = 0;
+i = lng;
+
+function dn() {  
+arr.splice(n+1,1);
+};
+
+function dm() {  
+arr.splice(n,1);
+};
+
+console.log(arr[n]);
+console.log(arr[n+2]);
+
+lng = arr.length;
+
+console.log(lng);
+
+n = n+2;
+}
+while (i > 2)
+
+console.log(arr);
+
+
 
