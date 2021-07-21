d52eb065209d868e1a4626073a03ae686f501df5
johnisthebest <48302154+johnisthebest@users.noreply.github.com>
2021-07-21 19:07:36 -0400

    Update kekkles.js

--- kekkles.js
+++ kekkles.js
@@ -1,56 +1,60 @@
-
-const CLIENT_ID = "81472223417c43ddba6f616bded26b4e"; // insert your client id here from spotify
-const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
-const REDIRECT_URL_AFTER_LOGIN = "http://johnzhao.ca";
-const SPACE_DELIMITER = "%20";
-const SCOPES = [
-  "user-read-currently-playing",
-  "user-read-playback-state",
-  "playlist-read-private",
-];
-const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER);
-
-/* 
-http://localhost:3000/webapp#access_token=ABCqxL4Y&token_type=Bearer&expires_in=3600
-*/
-const getReturnedParamsFromSpotifyAuth = (hash) => {
-  const stringAfterHashtag = hash.substring(1);
-  const paramsInUrl = stringAfterHashtag.split("&");
-  const paramsSplitUp = paramsInUrl.reduce((accumulater, currentValue) => {
-    console.log(currentValue);
-    const [key, value] = currentValue.split("=");
-    accumulater[key] = value;
-    return accumulater;
-  }, {});
-
-  return paramsSplitUp;
-};
-
-const WebApp = () => {
-  useEffect(() => {
-    if (window.location.hash) {
-      const { access_token, expires_in, token_type } =
-        getReturnedParamsFromSpotifyAuth(window.location.hash);
-
-      localStorage.clear();
-
-      localStorage.setItem("accessToken", access_token);
-      localStorage.setItem("tokenType", token_type);
-      localStorage.setItem("expiresIn", expires_in);
+(function() {
+    
+    function login(callback) {
+        var CLIENT_ID = ' 81472223417c43ddba6f616bded26b4e';
+        var REDIRECT_URI = 'http://johnzhao.ca';
+        function getLoginURL(scopes) {
+            return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
+              '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
+              '&scope=' + encodeURIComponent(scopes.join(' ')) +
+              '&response_type=token';
+        }
+        
+        var url = getLoginURL([
+            'user-read-email'
+        ]);
+        
+        var width = 450,
+            height = 730,
+            left = (screen.width / 2) - (width / 2),
+            top = (screen.height / 2) - (height / 2);
+    
+        window.addEventListener("message", function(event) {
+            var hash = JSON.parse(event.data);
+            if (hash.type == 'access_token') {
+                callback(hash.access_token);
+            }
+        }, false);
+        
+        var w = window.open(url,
+                            'Spotify',
+                            'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
+                           );
+        
     }
-  });
-const spthold = () => {
-  const handleLogin = () => {
-    window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
-  };
 
-  return (
-    <div className="container">
-      <h1>hi</h1>
-      <button onClick={handleLogin}>login to spotify</button>
-      <SpotifyGetPlaylists />
-    </div>
-  );
-};
+    function getUserData(accessToken) {
+        return $.ajax({
+            url: 'https://api.spotify.com/v1/me',
+            headers: {
+               'Authorization': 'Bearer ' + accessToken
+            }
+        });
+    }
 
-export default WebApp;
+    var templateSource = document.getElementById('result-template').innerHTML,
+        template = Handlebars.compile(templateSource),
+        resultsPlaceholder = document.getElementById('result'),
+        loginButton = document.getElementById('btn-login');
+    
+    loginButton.addEventListener('click', function() {
+        login(function(accessToken) {
+            getUserData(accessToken)
+                .then(function(response) {
+                    loginButton.style.display = 'none';
+                    resultsPlaceholder.innerHTML = template(response);
+                });
+            });
+    });
+    
+})();
